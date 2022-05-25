4f8c73dfc63d226d86c3d3a23f9f08b5e5921a34
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2022-05-25 21:54:45 +0200

    Modified utilities class

--- assets/js/game/utilities.js
+++ assets/js/game/utilities.js
@@ -1,9 +1,9 @@
 class Utilities {
     static transform_screen_isometric(position) {
-        var a = ( 0.50 * grid_size);
-        var b = (-0.50 * grid_size);
-        var c = ( 0.25 * grid_size);
-        var d = ( 0.25 * grid_size);
+        var a = ( 0.50 * 32);
+        var b = (-0.50 * 32);
+        var c = ( 0.25 * 32);
+        var d = ( 0.25 * 32);
 
         var new_a =  d;
         var new_b = -b;
@@ -11,17 +11,21 @@ class Utilities {
         var new_d =  a;
 
         var isometric = {
-            x: ((position.x * new_a) + (position.y * new_b) - grid_size * 0.5),
+            x: ((position.x * new_a) + (position.y * new_b) - 32 * 0.5),
             y: ( position.x * new_c) + (position.y * new_d)
         };
 
         isometric.x -= canvas_width * 0.5;
-        isometric.y -= canvas_height / (grid_size * 0.25);
+        isometric.y -= canvas_height * 0.5 - grid_size * 8;
 
         return isometric;
     }
 
     static screen_to_grid(position) {
         
+    }
+
+    static random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
