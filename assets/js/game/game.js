06f5ae3e85a9ba031d2e705575f57da08559a7dd
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2022-05-25 21:56:13 +0200

    Added a third imaginary dimension, modified input

--- assets/js/game/game.js
+++ assets/js/game/game.js
@@ -2,7 +2,9 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext("2d");
 var canvas_width = context.canvas.clientWidth;
 var canvas_height = context.canvas.clientHeight;
-var grid_size = 32;
+var grid_size = 16;
+var grid_height = 4;
+var image_size = 32;
 
 var UPS = 60;
 var Interval = 1000 / UPS;
@@ -10,37 +12,53 @@ var IntervalID;
 
 var grid = [];
 
+var input = new Vector();
+
 start_game();
 
 var player = new Player(
-    Math.floor(grid_size * 0.5),
-    Math.floor(grid_size * 0.5),
-    "./assets/img/player.png"
+    Math.floor(grid_size * 0.5) - 4,
+    Math.floor(grid_size * 0.5) - 4
 );
 
 document.addEventListener('keydown', function (event) {
-    var movement_speed = 1;
-    var input = new Vector();
+    if (event.repeat) return;
 
-    if (event.key == "w" || event.key == "W") {
-        input.x = -movement_speed;
-        input.y = -movement_speed;
-    } else if (event.key == "a" || event.key == "A") {
-        input.x = -movement_speed;
-        input.y =  movement_speed;
-    } else if (event.key == "s" || event.key == "S") {
-        input.x = movement_speed;
-        input.y = movement_speed;
-    } else if (event.key == "d" || event.key == "D") {
-        input.x =  movement_speed;
-        input.y = -movement_speed;
-    } else {
+    const key = event.key.toUpperCase();
+
+    switch (key) {
+        case Input.up:
+            input.x += -player.movement_speed;
+            input.y += -player.movement_speed;
+            break;
+        case Input.down:
+            input.x += player.movement_speed;
+            input.y += player.movement_speed;
+            break;
+        case Input.left:
+            input.x += -player.movement_speed;
+            input.y +=  player.movement_speed;
+            break;
+        case Input.right:
+            input.x +=  player.movement_speed;
+            input.y += -player.movement_speed;
+            break;
+        case Input.run:
+            player.movement_speed = 2;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.repeat) return;
+
+    const key = event.key.toUpperCase();
+    if (Input.any(key)) {
         input = new Vector();
     }
 
-    setInterval(() => {
-        player.move(input);
-    }, 1000/60);
+    if (key === Input.run) {
+        player.movement_speed = 1;
+    }
 });
 
 canvas.onmousemove = function (e) {
@@ -50,7 +68,6 @@ canvas.onmousemove = function (e) {
         y = Math.floor((e.clientY - rect.top) / grid_size);
 
     var position = Utilities.transform_screen_isometric({x: x, y: y});
-    console.log(position)
     //var tile = grid[position.x][position.y];
 };
 
@@ -76,9 +93,19 @@ function draw_background() {
 }
 
 function action_on_all_tiles(callback) {
-    for (var x = 0; x < grid_size; x++) {
-        for (var y = 0; y < grid_size; y++) {
-            callback(grid[x][y]);
+    for (var x = 0; x < grid.length; x++) {
+        for (var y = 0; y < grid[x].length; y++) {
+            for (var z = 0; z < grid[x][y].length; z++) {
+                callback(grid[x][y][z]);
+            }
+        }
+    }
+}
+
+function action_on_all_tiles_from_z(z, callback) {
+    for (var x = 0; x < grid.length; x++) {
+        for (var y = 0; y < grid[x].length; y++) {
+            callback(grid[x][y][z]);
         }
     }
 }
@@ -87,8 +114,10 @@ function update_tiles() {
     if (grid.length != 0) {
         for (var x = 0; x < grid.length; x++) {
             for (var y = 0; y < grid[x].length; y++) {
-                var tile = grid[x][y];
-                tile.update();
+                for (var z = 0; z < grid[x][y].length; z++) {
+                    var tile = grid[x][y][z];
+                    tile.update();
+                }
             }
         }
     }
@@ -97,6 +126,7 @@ function update_tiles() {
 function update_player() {
     if (player !== undefined) {
         player.update();
+        player.move(input);
     }
 }
 
@@ -104,8 +134,10 @@ async function dance_tiles() {
     if (grid.length != 0) {
         for (var x = 0; x < grid.length; x++) {
             for (var y = 0; y < grid[x].length; y++) {
-                var tile = grid[x][y];
-                await task(tile);
+                for (var z = 0; z < grid[x][y].length; z++) {
+                    var tile = grid[x][y][z];
+                    await task(tile);
+                }
             }
         }
     }
@@ -122,7 +154,29 @@ function start_game() {
     for (var x = 0; x < grid_size; x++) {
         grid[x] = [];
         for (var y = 0; y < grid_size; y++) {
-            grid[x][y] = new Tile(x, y);
+            grid[x][y] = [];
+            for (var z = 0; z < grid_height; z++) {
+                if (z === 0) {
+                    // First layer is grass
+                    grid[x][y][z] = new Grass(x + z, y + z);
+                } else if (z > 0 && z <= 2) {
+                    // Second and third layers are random between dirt and stone
+                    var r = Utilities.random(0,1);
+
+                    if (r === 0) {
+                        grid[x][y][z] = new Dirt(x + z * 2, y + z * 2);
+                    } else {
+                        grid[x][y][z] = new Stone(x + z * 2, y + z * 2);
+                    }
+
+                } else if (z > 2 && z < grid_height - 1) {
+                    // The rest is stone
+                    grid[x][y][z] = new Stone(x + z * 2, y + z * 2);
+                } else {
+                    // The rest is stone
+                    grid[x][y][z] = new Bedrock(x + z * 2, y + z * 2);
+                }
+            }
         }
     }
     if (IntervalID === undefined) {
