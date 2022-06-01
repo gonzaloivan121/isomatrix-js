3d45ce0f6bfa4836e9af874a08b6d6615ced0b7e
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2022-06-02 01:16:47 +0200

    Added player equipment and the ability to equip and unequip items

--- assets/js/game/entities/player.js
+++ assets/js/game/entities/player.js
@@ -1,8 +1,38 @@
 class Player extends Entity {
     inventory = new Inventory();
 
+    equipment = {
+        weapon: null,
+        shield: null,
+        gauntlet: null,
+        ring: null,
+        helmet: null,
+        chest_armour: null,
+        leg_armour: null,
+        boots: null,
+        pendant: null
+    }
+
     constructor(x, y, stats = new Stats()) {
         super(x, y, "./assets/img/entities/player.png");
         this.stats = stats;
+    }
+
+    equip_item(item) {
+        var type = ItemType.get_type(item.type);
+        if (this.equipment[type] === null) {
+            this.equipment[type] = item;
+        } else {
+            this.inventory.add_item_to_stack(this.equipment[type]);
+            this.equipment[type] = item;
+        }
+    }
+    
+    unequip_item(item) {
+        var type = ItemType.get_type(item.type);
+        if (!this.equipment[type] === null) {
+            this.inventory.add_item_to_stack(this.equipment[type]);
+            this.equipment[type] = null;
+        }
     }
 }
