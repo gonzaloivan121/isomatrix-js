f61feb53ceed475350251b57ad8d8377297db889
DIR\g.a.chaparro.barese <g.a.chaparro.barese@accenture.com>
2022-07-01 12:33:32 +0200

    Added equipment as new class

---
+++ assets/js/game/inventory/equipment.js
@@ -0,0 +1,13 @@
+class Equipment {
+    weapon = null;
+    shield = null;
+    gauntlet = null;
+    ring = null;
+    helmet = null;
+    chest_armour = null;
+    leg_armour = null;
+    boots = null;
+    pendant = null;
+
+    constructor() {}
+}
